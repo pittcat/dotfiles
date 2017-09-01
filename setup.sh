238eac4b3d2fb0da43a5172146fc770de2974d10
pittcat <pitt_wenchao@outlook.com>
2017-09-01 14:54:45 +0800

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+cp spacevim ~/.spacevim
+cp zshrc ~/.zshrc
+cp tmux.conf ~/.tmux.conf
+cp spacemacs ~/.spacemacs
+
+sudo apt install git vim zsh tmux python3-pip python-pip curl
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+git clone git://github.com/joelthelion/autojump.git
+cd autojump
+./install.py
+sudo rm autojump -r
