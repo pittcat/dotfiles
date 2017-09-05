03d1df2ec51a6ad0593080c36311770730954616
pittcat <pitt_wenchao@outlook.com>
2017-09-05 14:03:32 +0800

    add setup.sh
    Signed-off-by: pittcat <pitt_wenchao@outlook.com>

---
+++ setup.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+sudo apt install vim zsh tmux git curl python3-pip python-pip
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sudo pip3 install thefuck
+sudo apt install autojump
+git clone git://github.com/joelthelion/autojump.git
+cd autojump
+./install.py
+cd ..
+sudo rm autojump -r
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+sudo apt install xsel
+# prefix + I install
+
+
+#配置vim vim8 并且须支持python3
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/liuchengxu/space-vim/master/install.sh)"
+sh config.sh
+sudo apt-get install build-essential cmake
+sudo apt-get install python-dev python3-devsudo git clone https://github.com/Valloric/YouCompleteMe.git ~/.vim/plugged/YouCompleteMe
+git submodule update --init --recursive
+sudo python3 ./install.py --clang-completer
+sudo pip3 install yapf flake8
+sudo pip install yapf flake8
+sudo apt install ctags 
