0e2326455afc6112fdd205e753b9d0f06c0d389c
pittcat <pitt_wenchao@outlook.com>
2017-09-05 11:29:19 +0800

    add xsel

--- setup.sh
+++ setup.sh
@@ -5,10 +5,12 @@ cp zshrc ~/.zshrc
 cp tmux.conf ~/.tmux.conf
 cp spacemacs ~/.spacemacs
 
-sudo apt install git vim zsh tmux python3-pip python-pip curl
+sudo apt install git vim zsh tmux python3-pip python-pip curl ctags autojump
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 git clone git://github.com/joelthelion/autojump.git
 cd autojump
 ./install.py
+cd ..
 sudo rm autojump -r
+sudo pip3 install flake8
