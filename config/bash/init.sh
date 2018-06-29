6d5b8bda1f5769fd597ed5ecb0fef35bd1ccf048
pittcat <pitt_wenchao@outlook.com>
2018-06-29 00:08:16 +0000

    update

--- config/bash/init.sh
+++ config/bash/init.sh
@@ -1,5 +1,11 @@
+# bash lsp
 if hash yarn 2>/dev/null; then
   yarn global add bash-language-server
 else
   npm i -g bash-language-server
+fi
+
+if hash yaourt 2>/dev/null; then
+  # vim format
+  yaourt -S shfmt
 fi
