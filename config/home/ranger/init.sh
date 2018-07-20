a86fc9346a3a6a09062a457b5a310e01df9dde0b
pittcat <pitt_wenchao@outlook.com>
2018-07-20 15:17:00 +0000

    add ranger theme

--- config/home/ranger/init.sh
+++ config/home/ranger/init.sh
@@ -1,2 +1,12 @@
+#!/usr/bin/env sh
 sudo pacman -S ranger
+
+if [ -d $HOME/.config/ranger/colorschemes ];then
+  cp $PWD/colorthemes/bluish.py  $HOME/.config/ranger/colorschemes
+else
+  mkdir -p $HOME/.config/ranger/colorschemes
+  cp $PWD/colorthemes/bluish.py  $HOME/.config/ranger/colorschemes
+fi
+
 ln -s $PWD/rc.conf $HOME/.config/ranger/
+
