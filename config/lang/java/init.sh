50d5fdbf1b215fef1b65231d69934b610d30e323
pittcat <pitt_wenchao@outlook.com>
2018-08-02 03:17:28 +0000

    update

--- config/lang/java/init.sh
+++ config/lang/java/init.sh
@@ -2,7 +2,7 @@
 #!/usr/bin/env sh
 
 if hash trizen 2>/dev/null; then
-  trizen -S uncrustify jdtls google-java-format-git
+  trizen -S uncrustify jdtls google-java-format-git java-repl
 elseif
   echo "Please install trizen"
 fi
