3463c533c5a3d2c5d55186ec582830dabbde7ea8
pittcat <pitt_wenchao@outlook.com>
2018-08-13 04:54:59 +0800

    update i3 init.sh

--- i3/init.sh
+++ i3/init.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env sh
-sudo trizen -S conky rofi nautilus fcitx google-chrome rofi-greenclip xorg-xprop okular polybar dunst feh betterlockscreen  zeal tdrop-git
+trizen -S conky rofi nautilus fcitx google-chrome rofi-greenclip xorg-xprop okular dunst feh betterlockscreen  zeal tdrop-git polybar-git
 
-ln -s $PWD/i3 $HOME/.i3
+ln -s ../i3 $HOME/.i3
 ln -s $PWD/polybar $HOME/.config/    
-ln -s $PWD/i3/dunst/dunstrc ~/.config/dunst/dunstrc    
+mkdir -p $HOME/.config/dunst
+ln -s $PWD/dunst/dunstrc ~/.config/dunst/dunstrc    
 # Notice for polybar:
 # change the interface   
 # ip route | grep '^default' | awk '{print $5}'    
 # module/networkspeeddown    
 # interface = ens33 
-ln -s $PWD/i3/dunst $HOME/.config/
+# ln -s $PWD/i3/dunst $HOME/.config/
