3dafc7ed72bda4eef1dd7844e571d411bb6b5ac5
pittcat <pitt_wenchao@outlook.com>
2018-08-12 10:11:10 +0800

    update

--- manual.sh
+++ manual.sh
@@ -116,7 +116,7 @@ ln -s $PWD/tmux/tmux.conf ~/.tmux.conf
 # --------------------------------------------lang -------------------------------------------- #
 
 # python
-pip install 'python-language-server[all]' --user
+# pip install 'python-language-server[all]' --user
 # rust
 
 # terminmal daily use ------npm
