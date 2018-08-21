7e09eff0a31597a583b0871851e8f53a62f566e0
pittcat <pitt_wenchao@outlook.com>
2018-08-21 01:48:23 +0000

    update centos config

--- config/vps-init/centos/init.sh
+++ config/vps-init/centos/init.sh
@@ -8,6 +8,6 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 # config zsh
 mkdir -p $HOME/.antigen/
 curl -L git.io/antigen > $HOME/.antigen/antigen.zsh
-ln -s $PWD/zshrc ~/.zshrc
+cp $PWD/zshrc ~/.zshrc
 # config tmux
-ln -s $PWD/tmux.conf ~/.tmux.conf
+cp $PWD/tmux.conf ~/.tmux.conf
