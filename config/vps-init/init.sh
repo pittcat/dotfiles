aa954349b05b2d6e761ad50a406f8a5db4877336
pittcat <pitt_wenchao@outlook.com>
2018-08-22 08:58:41 +0000

    delete centos init

--- config/vps-init/init.sh
+++ config/vps-init/init.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
+if [ -d /etc/apt ];then
+  PKMG="apt install"
+elif [-d /etc/yum];then
+  PKMG="yum install"
+fi
 
-sudo yum install git zsh vim tmux
+sudo $PKMG git zsh vim tmux mosh iptables -y
 
 # install fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
@@ -8,6 +13,8 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 # config zsh
 mkdir -p $HOME/.antigen/
 curl -L git.io/antigen > $HOME/.antigen/antigen.zsh
-cp $PWD/zshrc ~/.zshrc
+curl https://raw.githubusercontent.com/pittcat/dotfiles/dotfiles/config/vps-init/centos/zshrc > ~/.zshrc
 # vimrc
 curl https://raw.githubusercontent.com/pittcat/dotfiles/dotfiles/config/vps-init/centos/vimrc > ~/.vimrc
+# mosh
+sudo iptables -I INPUT 1 -p udp --dport 60000:61000 -j ACCEPT
