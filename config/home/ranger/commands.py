4f8871b53fe2c05b1cfaa5f9b856c740c93c8d7d
pittcat <pitt_wenchao@outlook.com>
2018-09-03 03:45:12 +0000

    update ranger command.py

--- config/home/ranger/commands.py
+++ config/home/ranger/commands.py
@@ -66,3 +66,61 @@ class fzf_locate(Command):
                 self.fm.cd(fzf_file)
             else:
                 self.fm.select_file(fzf_file)
+
+
+class mkcd(Command):
+    """
+    :mkcd <dirname>
+
+    Creates a directory with the name <dirname> and enters it.
+    """
+
+    def execute(self):
+        from os.path import join, expanduser, lexists
+        from os import makedirs
+        import re
+
+        dirname = join(self.fm.thisdir.path, expanduser(self.rest(1)))
+        if not lexists(dirname):
+            makedirs(dirname)
+
+            match = re.search('^/|^~[^/]*/', dirname)
+            print(match)
+            if match:
+                self.fm.cd(match.group(0))
+                dirname = dirname[match.end(0):]
+
+            for m in re.finditer('[^/]+', dirname):
+                s = m.group(0)
+                if s == '..' or (s.startswith('.')
+                                 and not self.fm.settings['show_hidden']):
+                    self.fm.cd(s)
+                else:
+                    self.fm.thisdir.load_content(schedule=False)
+                    self.fm.execute_console('scout -ae ^{}$'.format(s))
+        else:
+            self.fm.notify("file/directory exists!", bad=True)
+
+
+class newfile_with_shebang(Command):
+    def execute(self):
+        from os.path import join, expanduser, lexists
+        from os import mknod
+
+        filename = join(self.fm.thisdir.path, expanduser(self.rest(1)))
+        if not lexists(filename):
+            mknod(filename)
+            all_shebang = {
+                "c": "#include <stdio.h>",
+                "sh": "#!/usr/bin/env bash",
+                "cpp": "#include <iostream>\nusing namespace std;",
+                "py": "#!/usr/bin/env python\n# -*- coding: utf-8 -*-"
+            }
+
+            if all_shebang.get(filename.split(".")[-1]):
+                f = open(filename, 'w')
+                f.write(all_shebang.get(filename.split(".")[-1]))
+                f.close()
+
+        else:
+            self.fm.notify("file/directory exists!", bad=True)
