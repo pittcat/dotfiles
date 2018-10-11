0e9312d399440adc7ac7e73c75c3e174510ceb9a
pittcat <pitt_wenchao@outlook.com>
2018-10-11 11:12:53 +0000

    update emacs config

--- config/home/emacs/lisp/pittcat-org-moe.el
+++ config/home/emacs/lisp/pittcat-org-moe.el
@@ -3,4 +3,30 @@
 (global-set-key (kbd "C-SPC") 'nil)
 
 
+;;show-img
+;; -- Display images in org mode
+;; enable image mode first
+(iimage-mode)
+;; add the org file link format to the iimage mode regex
+(add-to-list 'iimage-mode-image-regex-alist
+  (cons (concat "\\[\\[file:\\(~?" iimage-mode-image-filename-regex "\\)\\]")  1))
+;;  add a hook so we can display images on load
+(add-hook 'org-mode-hook '(lambda () (org-turn-on-iimage-in-org)))
+;; function to setup images for display on load
+(defun org-turn-on-iimage-in-org ()
+  "display images in your org file"
+  (interactive)
+  (turn-on-iimage-mode)
+  (set-face-underline-p 'org-link nil))
+;; function to toggle images in a org bugger
+(defun org-toggle-iimage-in-org ()
+  "display images in your org file"
+  (interactive)
+  (if (face-underline-p 'org-link)
+      (set-face-underline-p 'org-link nil)
+      (set-face-underline-p 'org-link t))
+  (call-interactively 'iimage-mode))
+
+(setq org-image-actual-width (/ (display-pixel-width) 3))
+
 (provide 'pittcat-org-moe)
