f60595019011ec034a70dec6cf8ed7b19db35812
pittcat <pitt_wenchao@outlook.com>
2019-02-05 00:20:20 +0800

    update

---
+++ i3/init.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+yay -S conky rofi nautilus fcitx google-chrome rofi-greenclip xorg-xprop okular dunst feh betterlockscreen  zeal tdrop-git polybar-git
+
+ln -s ../i3 $HOME/.i3
+ln -s $PWD/polybar $HOME/.config/    
+mkdir -p $HOME/.config/dunst
+ln -s $PWD/dunst/dunstrc ~/.config/dunst/dunstrc    
+# Notice for polybar:
+# change the interface   
+# ip route | grep '^default' | awk '{print $5}'    
+# module/networkspeeddown    
+# interface = ens33 
+# ln -s $PWD/i3/dunst $HOME/.config/
