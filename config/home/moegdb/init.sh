3626034aed3da931ac0b62be0f27f97fc3a4b7a6
pittcat <pitt_wenchao@outlook.com>
2019-04-11 16:35:47 +0800

    update gdb

--- config/home/moegdb/init.sh
+++ config/home/moegdb/init.sh
@@ -3,7 +3,7 @@
 sudo pacman -S cgdb
 pip install gdbgui --user
 ln -s $PWD/cgdbrc ~/.cgdb
-# wget -P ~ git.io/.gdbinit
+wget -O -  git.io/.gdbinit>~/.gdbinit-dashboard
 
 wget -O ~/.gdbinit-gef.py -q https://github.com/hugsy/gef/raw/master/gef.py
 sudo cp $PWD/gef /usr/local/bin
