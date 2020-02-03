fe4275fe3528e3f811b6928b976a3bcba4fce755
pittcat <pitt_wenchao@outlook.com>
2020-02-03 23:50:13 +0800

    update

--- config/home/tags/init.zsh
+++ config/home/tags/init.zsh
@@ -2,8 +2,9 @@
 yay -S universal-ctags-git cscope global
 pip install pygments --user
 mkdir -p ~/.ctags.d/
-ln -s $PWD/tags ~/.ctags.d/.ctags
 
 ln -s $PWD/ctags ~/.ctags
+if [ -d "/usr/share/gtags/gtags.conf" ] ; then
 sudo rm /usr/share/gtags/gtags.conf
+fi
 sudo ln -s $PWD/gtags.conf /usr/share/gtags
