5148bd2fad950ecb8140517a8b3a3130d6d55cea
pittcat <pitt_wenchao@outlook.com>
2021-06-14 09:05:22 +0800

    update

--- config/lang/latex/init.sh
+++ config/lang/latex/init.sh
@@ -1,3 +1,3 @@
-yay -S texlive-most  texlive-lang  biber pplatex-git evince
+yay -S texlive-most  texlive-lang  biber pplatex-git
 yarn global add textlint
 pip3 install neovim-remote
--- homeconf/.config/i3/bing-wallpaper.sh
+++ homeconf/.config/i3/bing-wallpaper.sh
@@ -5,10 +5,10 @@ if [[ ! -d "~/Image" ]]; then
 fi
 
 imagepath="$HOME/Image/bing-wallpaper.jpg"
-urlpath=$( \
-curl "https://www.bing.com/HPImageArchive.aspx?format=rss&idx=0&n=1&mkt=en-US" \
-| xmllint --xpath "/rss/channel/item/link/text()" - \
-| sed 's/1366x768/1920x1080/g' \
-)
-curl "https://www.bing.com$urlpath" -o $imagepath
+# urlpath=$( \
+# curl "https://www.bing.com/HPImageArchive.aspx?format=rss&idx=0&n=1&mkt=en-US" \
+# | xmllint --xpath "/rss/channel/item/link/text()" - \
+# | sed 's/1366x768/1920x1080/g' \
+# )
+# curl "https://www.bing.com$urlpath" -o $imagepath
 feh --bg-fill $imagepath
