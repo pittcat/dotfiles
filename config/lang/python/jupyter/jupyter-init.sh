f002254f402c5ba439b61c684948c880de896cb5
pittcat <pitt_wenchao@outlook.com>
2021-07-21 01:59:08 +0800

    add jupytertheme for jupyter

--- config/lang/python/jupyter/jupyter-init.sh
+++ config/lang/python/jupyter/jupyter-init.sh
@@ -3,4 +3,8 @@
 sudo pacman -S pandoc python-qtconsole jupyter jupytext
 sudo pip install jupyter pyecharts 
 
+pip install --upgrade jupyterthemes
+
+jt -t onedork -T -vim -nfs 16 -fs 16  -f firacode -cellw 92%
+
 ln -s $PWD/jupyter_qtconsole_config.py ~/.jupyter/jupyter_qtconsole_config.py
